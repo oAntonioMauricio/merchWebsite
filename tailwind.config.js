29d2112a3d99436690f28890a16511ba2dc703b4
António <oantoniomauricio@gmail.com>
2022-07-11 17:09:02 +0100

    tailwind script

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    "tailwindCSS.includeLanguages": {
+        "blade": "html",
+    },
+  }
+  
